ca5122b91d9f999b1477c196852f74060d339df3
fpmm-mm-11 <fpmm-mm-11-git@mail.ru>
2016-04-18 22:38:46 +0500

    First commit

---
+++ TestShop/TestShop/Model/ВидТовара.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestShop.Model
+{
+    public class ВидТовара
+    {
+        private long id;
+
+        private string наименование;
+
+        /// <summary>
+        /// Уникальный идентификатор объекта
+        /// </summary>
+        public virtual long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// Наименование вида товара
+        /// </summary>
+        public virtual string Наименование
+        {
+            get { return наименование; }
+            set { наименование = value; }
+        }
+    }
+}
---
+++ TestShop/TestShop/Services/DBService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Cfg;
+using TestShop.Model;
+
+namespace TestShop.Services
+{
+    public class DBService
+    {
+        public static void Save(ВидТовара видТовара)
+        {
+            try
+            {
+                Configuration configuration = new Configuration();
+                configuration.Configure();
+
+                ISessionFactory factory = configuration.BuildSessionFactory();
+
+                ISession session = factory.OpenSession();
+                ITransaction transaction = session.BeginTransaction();
+
+                session.SaveOrUpdate(видТовара);
+
+                transaction.Commit();
+
+                session.Close();
+                session.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
